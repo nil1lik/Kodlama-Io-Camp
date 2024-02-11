9ad38eef93d819f41e3211558a5a2affbf2f1bd7
Nil <144815514+nil1lik@users.noreply.github.com>
2024-02-11 21:23:53 +0300

    Day 2 Lesson 1

---
+++ Intro/Intro/Entities/CorporateCustomer.cs
@@ -0,0 +1,8 @@
+﻿namespace Intro.Entities
+{
+    public class CorporateCustomer:BaseCustomer //Kurumsal Müşteri
+    {
+        public string Name { get; set; }
+        public string TaxNumber { get; set; }
+    }
+}
--- Intro/Intro/Program.cs
+++ Intro/Intro/Program.cs
@@ -64,4 +64,59 @@ for (int i = 0; i < courses2.Length; i++)
 
 Console.WriteLine("Kod Bitti.");
 
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678910";
+customer1.FirstName = "Aslı";
+customer1.LastName = "Karayiğit";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer1.Id = 2;
+customer1.NationalIdentity = "56789103544";
+customer1.FirstName = "Özgür";
+customer1.LastName = "Atılgan";
+customer1.CustomerNumber = "123457";
+
+CorporateCustomer customer3 = new CorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlamaio";
+customer3.CustomerNumber = "654778";
+customer3.TaxNumber = "1234567890";
+
+CorporateCustomer customer4 = new CorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "Abc";
+customer4.CustomerNumber = "654779";
+customer4.TaxNumber = "1234567897";
+
+int number1 = 10;
+int number2 = 20;
+number1 = number2;
+number2 = 50;
+Console.WriteLine(number1);
+
+
+string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+string[] cities2 = { "Bursa", "Bolu", "Diyarbakır" };
+
+cities2 = cities1;
+cities1[0] = "Adana";
+
+Console.WriteLine(cities2[0]);
+
+//value types ==> int, bool, double...
+//reference types ==> array, class, interface...
+                                //101       102       103        104
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+
+//polymorphism
+foreach (BaseCustomer customer in customers)
+{
+    if (customer is IndividualCustomer)
+    {
+        (IndividualCustomer)customer.
+    }
+    Console.WriteLine(customer.CustomerNumber);
+}
 
